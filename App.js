e89c886b375e4578a872fa61e2afd2a373775685
Tooga Garanja Magar <tugamagar77@gmail.com>
2023-09-15 13:06:55 -0400

    Task3: background dark and white

--- App.js
+++ App.js
@@ -5,20 +5,27 @@ import { Button, Text, TextInput, StyleSheet, View } from 'react-native';
 export default function BasicApp() {
 
   const [value, onChangeText] = useState('Placeholder');
+  const [backgroundColor, setBackgroundColor] = useState('white');
+
+  const isLightBackground = backgroundColor === 'white';
+
+  const handleBackgroundChange = (color) => {
+    setBackgroundColor(color);
+  };
 
   return (
-    <View style={{ ...styles.container }}>
+    <View style={{ ...styles.container, backgroundColor }}>
 
-      <Text style={styles.header}>TOOGA Basic App</Text>
-
+        <Text style={styles.header}>Tooga Basic App</Text>
 
       <TextInput style={styles.textinput}
         onChangeText={text => onChangeText(text)}
         value={value} clearTextonFocus='false'
       />
 
-    <Button title='Clear' onPress={() => onChangeText('')} />
+      <Button title='Clear' onPress={() => onChangeText('')} />
 
+          <Button title={isLightBackground ? 'Change Background' : 'Change Background'} onPress={() => handleBackgroundChange(isLightBackground ? 'black' : 'white')} />
 
       <Text style={styles.text}> {value} </Text>
 
